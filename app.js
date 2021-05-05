5d204d71995a2f9515b6c24db448f74eeaff287c
nicolaijohnsen <nico7711@outlook.dk>
2021-05-05 13:23:37 +0200

    all completed pages are now fully optimised for resizing. services.html is almost finished

---
+++ app.js
@@ -0,0 +1,24 @@
+const burgerMenu = 'images/burger-menu-icon.svg'
+const exitMenu = 'images/menu-exit.svg'
+let navCounter = 1;
+
+
+
+document.querySelector('.burger-menu').addEventListener('click', function(e) {
+    const secondElement = document.body.children[1];
+    let nav = document.querySelector('.super-nav');
+    let slideInNav = document.querySelector('.slide-in-nav')
+    if (navCounter % 2 === 1) { //slide in
+        nav.style.position = 'fixed';
+        slideInNav.style.transform = 'translateX(0%)';
+        e.target.src = exitMenu
+        secondElement.style.marginTop = '100px';
+
+    } else { // slide out
+        nav.style.position = 'relative';
+        slideInNav.style.transform = 'translateX(100%)';
+        e.target.src = burgerMenu
+        secondElement.style.marginTop = '0px'
+    }
+    navCounter++
+})
