e398834ec24289a2dce4598692624be85116d230
nicolaijohnsen <nico7711@outlook.dk>
2021-05-11 12:03:18 +0200

    input fields now change to white background when not null. mobile slide in navigation disappears when clicking outside of the slide in navigation element. Refactored JS.

--- app.js
+++ app.js
@@ -1,26 +1,64 @@
-const burgerMenu = 'images/burger-menu-icon.svg'
-const exitMenu = 'images/menu-exit.svg'
-let navCounter = 1;
+const burgerMenu = document.querySelector('.burger-menu')
+const burgerMenuPath = 'images/burger-menu-icon.svg'
+const exitMenuPath = 'images/menu-exit.svg'
+const secondDOMElement = document.body.children[1];
+const nav = document.querySelector('.super-nav');
+const slideInNav = document.querySelector('.slide-in-nav')
+// TO DOs
+
 
 /**
  * Slide in navigation bar on mobile
  */
-document.querySelector('.burger-menu').addEventListener('click', function(e) {
-    const secondElement = document.body.children[1];
-    let nav = document.querySelector('.super-nav');
-    let slideInNav = document.querySelector('.slide-in-nav')
-    if (navCounter % 2 === 1) { //slide in
-        nav.style.position = 'fixed';
-        slideInNav.style.transform = 'translateX(0%)';
-        e.target.src = exitMenu
-        secondElement.style.marginTop = '100px';
 
-    } else { // slide out
-        nav.style.position = 'relative';
-        slideInNav.style.transform = 'translateX(100%)';
-        e.target.src = burgerMenu
-        secondElement.style.marginTop = '0px'
+function getParentElement(element) {
+    if (element.parentElement !== document.body) {
+        return getParentElement(element.parentElement)
     }
-    navCounter++
+    return element
+}
+
+const slideOutByBodyClick = (e) => {
+    if (getParentElement(e.target) !== document.querySelector('.super-nav')) {
+        slideOut()
+    }
+}
+
+const slideOut = () => {
+    nav.style.position = 'relative';
+    slideInNav.style.transform = 'translateX(100%)';
+    secondDOMElement.style.marginTop = '0px'
+    burgerMenu.setAttribute("src", burgerMenuPath)
+
+    document.body.removeEventListener('click', slideOutByBodyClick)
+}
+
+const slideIn = ()=> {
+    nav.style.position = 'fixed';
+    slideInNav.style.transform = 'translateX(0%)';
+    secondDOMElement.style.marginTop = '100px';
+    burgerMenu.setAttribute("src", exitMenuPath)
+}
+
+burgerMenu.addEventListener('click', function() {
+    if (burgerMenu.getAttribute("src") === burgerMenuPath) { //slide in
+        slideIn()
+        document.body.addEventListener('click', slideOutByBodyClick, {capture: true})
+    } else { // slide out
+        slideOut()
+    }
 })
+
+// Change background color on input fields from grey to white when non-empty, and back again when empty.
+document.querySelectorAll('input[type="text"]').forEach(input => {
+    input.addEventListener('change', function (e) {
+        if (e.target.value !== "") {
+            e.target.style.background = 'white'
+        } else {
+            e.target.style.background = '#40676E11'
+        }
+    })
+})
+
+
 
